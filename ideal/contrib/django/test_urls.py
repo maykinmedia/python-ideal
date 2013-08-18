37cbc00b85cc515cc5ccaa80cfe41bee1f2e4f7e
Joeri Bekker <joeri@maykinmedia.nl>
2013-08-18 20:48:03 +0200

    Added Django integration.

---
+++ ideal/contrib/django/test_urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
+
+from ideal.contrib.django.views import GetIssuersView, StartTransactionView, GetTransactionStatusView
+
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='ideal/tests/index.html'), name='ideal_tests_index'),
+    url(r'^get_issuers/$', GetIssuersView.as_view(), name='ideal_tests_get_issuers'),
+    url(r'^start_transaction/$', StartTransactionView.as_view(), name='ideal_tests_start_transaction'),
+    url(r'^get_transaction_status/$', GetTransactionStatusView.as_view(), name='ideal_tests_get_transaction_status'),
+)
---
+++ ideal/contrib/django/urls.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
+
+# urlpatterns = patterns('',
+#     (r'^$', TemplateView.as_view(template_name='master.html')),
+# )
