0ae020cad2a5a9e88548dc32cfa5f2c09137fb97
Joeri Bekker <joeri@maykinmedia.nl>
2013-10-08 13:46:55 +0200

    Added settings to Django index template.

--- ideal/contrib/django/ideal_compat/test_urls.py
+++ ideal/contrib/django/ideal_compat/test_urls.py
@@ -4,13 +4,11 @@ if django.VERSION[0:1] < (1,4):
 else:
     from django.conf.urls import patterns, url
 
-from django.views.generic.base import TemplateView
-
-from ideal.contrib.django.ideal_compat.views import GetIssuersView, StartTransactionView, GetTransactionStatusView
+from ideal.contrib.django.ideal_compat.views import IndexView, GetIssuersView, StartTransactionView, GetTransactionStatusView
 
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='ideal/tests/index.html'), name='ideal_tests_index'),
+    url(r'^$', IndexView.as_view(), name='ideal_tests_index'),
     url(r'^get_issuers/$', GetIssuersView.as_view(), name='ideal_tests_get_issuers'),
     url(r'^start_transaction/$', StartTransactionView.as_view(), name='ideal_tests_start_transaction'),
     url(r'^get_transaction_status/$', GetTransactionStatusView.as_view(), name='ideal_tests_get_transaction_status'),
