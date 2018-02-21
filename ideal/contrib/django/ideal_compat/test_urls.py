18ace6314240e224b2067d59fbf2cc6d94c3d03f
Martijn Jacobs <martijn@devopsconsulting.nl>
2018-02-21 12:58:28 +0100

    Just import views

--- ideal/contrib/django/ideal_compat/test_urls.py
+++ ideal/contrib/django/ideal_compat/test_urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 
-from ideal.contrib.django.ideal_compat.views import (
-    GetIssuersView, GetTransactionStatusView, IndexView, StartTransactionView
-)
+from ideal.contrib.django.ideal_compat import views
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='ideal_tests_index'),
-    url(r'^get_issuers/$', GetIssuersView.as_view(), name='ideal_tests_get_issuers'),
-    url(r'^start_transaction/$', StartTransactionView.as_view(), name='ideal_tests_start_transaction'),
-    url(r'^get_transaction_status/$', GetTransactionStatusView.as_view(), name='ideal_tests_get_transaction_status'),
+    url(r'^$', views.IndexView.as_view(), name='ideal_tests_index'),
+    url(r'^get_issuers/$', views.GetIssuersView.as_view(), name='ideal_tests_get_issuers'),
+    url(r'^start_transaction/$', views.StartTransactionView.as_view(), name='ideal_tests_start_transaction'),
+    url(
+        r'^get_transaction_status/$', views.GetTransactionStatusView.as_view(),
+        name='ideal_tests_get_transaction_status'),
 ]
